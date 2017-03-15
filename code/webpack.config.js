fbb5e88666d2501489faae6c06911ae37ea8307b
jfarah <julio.farah@gmail.com>
2017-03-15 09:21:38 -0300

    configure react presets

--- code/webpack.config.js
+++ code/webpack.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   entry: [ './app.js' ],
   output: {
-      filename: './public/bundle.js'
-    },
+    filename: './public/bundle.js'
+  },
   module: {
-      loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/  },
-          ]
-    }
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/  },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/  }
+    ]
+  }
 }
